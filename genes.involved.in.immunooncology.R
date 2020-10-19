7f3de67b2ce80c37b664df038b2c1ef9573002bf
ZedaoLiu <liuzedaosk@163.com>
2020-10-19 14:29:43 +0800

    Update genes.involved.in.immunooncology.R

--- genes.involved.in.immunooncology.R
+++ genes.involved.in.immunooncology.R
@@ -5,10 +5,10 @@ conventional.T.cell.markers <- c(
   'FOXP3'
 )
 
-cytotoxic.markers <- c(
-  'GZMA',
-  'GZMB',
-  'GZMK',
+effector.markers <- c(
+  'GZMA', # granzyme expression during the differentiation process of memory CD8+ T cells might be as follows: GrA+/GrB–/GrK+ ? GrA+/GrB+/GrK+ ? GrA+/GrB+/GrK– [12]
+  'GZMB', # granzyme expression during the differentiation process of memory CD8+ T cells might be as follows: GrA+/GrB–/GrK+ ? GrA+/GrB+/GrK+ ? GrA+/GrB+/GrK– [12]
+  'GZMK', # granzyme expression during the differentiation process of memory CD8+ T cells might be as follows: GrA+/GrB–/GrK+ ? GrA+/GrB+/GrK+ ? GrA+/GrB+/GrK– [12]
   'GZMH',
   'GNLY',
   'PRF1',
@@ -26,6 +26,14 @@ naive.markers <- c(
   ""
 )
 
+memory.markers <- c(
+  "CD44", # cell adhesion molecule, expressed "in many cell types" [8]. expressed on almost all T cells [3]. indicated as 'effector-memory T cell marker' [8, 9]
+  "CCR5", # breast cancer expression & target, involved in HIV. T cell function not well established [10]. expression related to but not well-distinguish Tem [2, 3]. Suggested as Tem marker 
+)
+
+
+  
+
 # references:
 # 1. Landscape of infiltrating T cells in liver cancer revealed by single-cell sequencing, Cell, 2017
 # 2. Global characterization of T cells in non-small-cell lung cancer by single-cell sequencing, Nature Medicine, 2018
@@ -34,3 +42,10 @@ naive.markers <- c(
 # 5. CD8+ T cell states in human cancer: insights from single-cell analysis, Nature review cancer, 2020
 # 6. Clonal replacement of tumor-specific T cells following PD-1 blockade, Nature medicine, 2019
 # 7. https://www.wikigenes.org/e/gene/e/4118.html
+# 8. https://en.wikipedia.org/wiki/CD44
+# 9. Memory T Cell Subsets, Migration Patterns, and Tissue Residence, annual review immunology, 2013
+# 10. https://en.wikipedia.org/wiki/CCR5
+# 11. Three Memory Subsets of Human CD8 T Cells Differently Expressing Three Cytolytic Effector Molecules, 
+# 12. Differential expression of human granzymes A, B, and K in natural killer cells and during CD8+ T cell differentiation in peripheral blood
+
+
