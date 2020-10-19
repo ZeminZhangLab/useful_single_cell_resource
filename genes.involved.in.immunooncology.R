15022bf1deb4e931831abcb5dd376fa6e15038cc
ZedaoLiu <liuzedaosk@163.com>
2020-10-19 13:16:57 +0800

    Create genes.involved.in.immunooncology.R

---
+++ genes.involved.in.immunooncology.R
@@ -0,0 +1,16 @@
+conventional.T.cell.markers <- c(
+  'CD3D',
+  'CD4',
+  'CD8A',
+  'FOXP3'
+)
+
+cytotoxic.markers <- c(
+  'GZMA',
+  'GZMB',
+  'GZMK',
+  'GZMH',
+  'GNLY',
+  'PRF1',
+  'IFNG'
+)
