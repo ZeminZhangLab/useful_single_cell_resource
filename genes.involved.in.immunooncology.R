72e993c9e1016b16c8933596a8a1678caa0c0f08
ZedaoLiu <liuzedaosk@163.com>
2020-10-19 13:44:20 +0800

    Update genes.involved.in.immunooncology.R

--- genes.involved.in.immunooncology.R
+++ genes.involved.in.immunooncology.R
@@ -14,3 +14,19 @@ cytotoxic.markers <- c(
   'PRF1',
   'IFNG'
 )
+
+naive.markers <- c(
+  "LEF1",
+  "CCR7",
+  "SELL",
+  "TCF7",
+  
+  "IL7R", # in pan-T ranked as a marker. However, expressed in almost all cd8&4 except exhausted [1~3]. involved in VDJ recombination [4]. Don't suggest to use when assigning clusters. Could use in retrospection.
+  ""
+)
+
+# references:
+# 1. Landscape of infiltrating T cells in liver cancer revealed by single-cell sequencing, Cell, 2017
+# 2. Global characterization of T cells in non-small-cell lung cancer by single-cell sequencing, Nature Medicine, 2018
+# 3. Lineage tracking reveals dynamic relationships of T cells in colorectal cancer, Nature, 2018
+# 4. https://en.wikipedia.org/wiki/Interleukin-7_receptor
