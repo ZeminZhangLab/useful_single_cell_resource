0eed9f9c11fff891334cf31b526f84efd07d43c8
ZedaoLiu <liuzedaosk@163.com>
2020-10-19 14:03:12 +0800

    Update genes.involved.in.immunooncology.R

--- genes.involved.in.immunooncology.R
+++ genes.involved.in.immunooncology.R
@@ -21,6 +21,7 @@ naive.markers <- c(
   "SELL",
   "TCF7",
   
+  "MAL", # correlates surprisingly well (tSNE gene expression) with CCR7 [1~3], functions in immunology not completely understood but well summarized[7]. worth further looking at if have time. Biomarker in treatment response a story? 
   "IL7R", # in pan-T ranked as a marker. However, expressed in almost all cd8&4 except exhausted [1~3]. involved in VDJ recombination [4]. suggested in Melanoma & BCC, could be skin-cancer enriched? [5] but not validated in [6]. Don't suggest to use when assigning clusters. Could use in retrospection. In fact, almost useless in defining Naive.
   ""
 )
@@ -32,4 +33,4 @@ naive.markers <- c(
 # 4. https://en.wikipedia.org/wiki/Interleukin-7_receptor
 # 5. CD8+ T cell states in human cancer: insights from single-cell analysis, Nature review cancer, 2020
 # 6. Clonal replacement of tumor-specific T cells following PD-1 blockade, Nature medicine, 2019
-
+# 7. https://www.wikigenes.org/e/gene/e/4118.html
