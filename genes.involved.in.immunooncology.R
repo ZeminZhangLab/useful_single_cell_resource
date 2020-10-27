c1a924ce0b024f4dcd1f30876477e16bbf12807b
ZedaoLiu <liuzedaosk@163.com>
2020-10-27 09:48:09 +0800

    Update genes.involved.in.immunooncology.R

--- genes.involved.in.immunooncology.R
+++ genes.involved.in.immunooncology.R
@@ -93,12 +93,17 @@ t.cell.transcription.factors <- c(
   
   # it seems easy to discover a lot of these things using 'correlation in transcriptome', starting from some keynote genes such as TOX, TCF7
   
+  # below are 'established'
   "TCF7", # expressed exclusively in 'progenitor exhausted' [24] this conclusion seem to be supported by many many studies and TCF7 subset associates with improved prognosis. [25~27]
-  "EOMES", # TODO
   "TOX", # highly expressed in dysfuntional T cells in tumor and exhaustive T cells in chronic viral infection. high expression of TOX correlated with high expression of inhibitory receptors and low expression of TCF7[28] validated in [6]. also expressed in Treg & CD4 Tex [1, 6] plays in the elegant control of long-time T cell activation (exhaustion), have suggested functions in TCF1 cells [30] dispensable for Tem but essential for Tex [31]
+  
+  # below are less validated
   "NFATC2", # encodes NFAT1 [29], NFAT1 claimed to regulate TOX [28]. NFATs expression not that differential [1]
   "SETBP1", # identified in [31] to correlate with TOX. acts like a DNA-binding protein [32] express but not significantly in Tex & Treg [1, 3] (low detection reasonable, given being low-expression DNA binding proteins). Might be upstream regulators of TOX, etc. seems not very important.
   "IKZF2",  # identified in [31] to correlate with TOX. zinc finger as name suggests, involved in hematopoietic development [33] 
+  "NR4A2", # claimed to be expressed in Tex [34], validated but not that significantly or consistently across datasets [1~3]
+  "MAF", # claimed to be expressed in Tex [34], validated but not that significantly or consistently across datasets [1~3]
+  "EOMES", # TODO
 )
 
 other.genes.DNA.binding <- c(
@@ -138,3 +143,4 @@ other.genes.DNA.binding <- c(
 # 31. TOX transcript+ionally and epigenetically programs CD8 T cell exhaustion, Nature, 2019
 # 32. https://en.wikipedia.org/wiki/SETBP1
 # 33. https://en.wikipedia.org/wiki/IKZF2
+# 34. Molecular profiling of CD8 T cells in autochthonous melanoma identifies Maf as driver of exhaustion, EMBO 2015
