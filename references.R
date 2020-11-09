8303a2aaee6fd442c6d5cf25b5cc33f2635b8794
ZedaoLiu <liuzedaosk@163.com>
2020-11-09 14:37:36 +0800

    Create references.R

---
+++ references.R
@@ -0,0 +1,80 @@
+
+# references:
+# 1. Landscape of infiltrating T cells in liver cancer revealed by single-cell sequencing, Cell, 2017
+# 2. Global characterization of T cells in non-small-cell lung cancer by single-cell sequencing, Nature Medicine, 2018
+# 3. Lineage tracking reveals dynamic relationships of T cells in colorectal cancer, Nature, 2018
+# 4. https://en.wikipedia.org/wiki/Interleukin-7_receptor
+# 5. CD8+ T cell states in human cancer: insights from single-cell analysis, Nature review cancer, 2020
+# 6. Clonal replacement of tumor-specific T cells following PD-1 blockade, Nature medicine, 2019
+# 7. https://www.wikigenes.org/e/gene/e/4118.html
+# 8. https://en.wikipedia.org/wiki/CD44
+# 9. Memory T Cell Subsets, Migration Patterns, and Tissue Residence, annual review immunology, 2013
+# 10. https://en.wikipedia.org/wiki/CCR5
+# 11. Three Memory Subsets of Human CD8 T Cells Differently Expressing Three Cytolytic Effector Molecules, 
+# 12. Differential expression of human granzymes A, B, and K in natural killer cells and during CD8+ T cell differentiation in peripheral blood
+# 13. pan-T zhanglab
+# 14. https://en.wikipedia.org/wiki/CXCR3
+# 15. https://en.wikipedia.org/wiki/CXCR4
+# 16. https://en.wikipedia.org/wiki/CCL4
+# 17. https://www.genecards.org/cgi-bin/carddisp.pl?gene=GPR183
+# 18. Memory T Cell Subsets, Migration Patterns, and Tissue Residence, Annual review
+# 19. https://www.genecards.org/cgi-bin/carddisp.pl?gene=ZNF683
+# 20. (2015). Functional classification of memory CD8(+) T cells by CX3CR1 expression. Nat. Commun.
+# 21. https://en.wikipedia.org/wiki/KLRG1
+# 22. Clonal replacement of tumor-specific T cells following PD-1 blockade, Nat Med 2019
+# 23. Follicular Helper CD4 T Cells (TFH), Annual Reviews immunology, 2011
+# 24. Subsets of exhausted CD8+ T cells differentially mediate tumor control and respond to checkpoint blockade, Nature immunology 2019
+# 25. Defining T Cell States Associated with Response to Checkpoint Immunotherapy in Melanoma, Cell, 2018
+# 26. Defining CD8+ T cells that provide the proliferative burst after PD-1 therapy, Nature, 2016
+# 27. Intratumoral Tcf1+PD-1+CD8+ T Cells with Stem-like Properties Promote Tumor Control in Response to Vaccination and Checkpoint Blockade Immunotherapy, Immunity, 2019
+# 28. TOX is a critical regulator of tumour-specific T cell differentiation, Nature, 2019
+# 29. https://www.cellsignal.com/products/primary-antibodies/nfat1-antibody/4389
+# 30. TOX reinforces the phenotype and longevity of exhausted T cells in chronic viral infection, Nature, 2019
+# 31. TOX transcript+ionally and epigenetically programs CD8 T cell exhaustion, Nature, 2019
+# 32. https://en.wikipedia.org/wiki/SETBP1
+# 33. https://en.wikipedia.org/wiki/IKZF2
+# 34. Molecular profiling of CD8 T cells in autochthonous melanoma identifies Maf as driver of exhaustion, EMBO 2015
+# 35. High Levels of Eomes Promote Exhaustion of Anti-tumor CD8+ T Cells, Trends in immunology
+# 36. NKG7 makes a better killer, Nature immunology, 2019
+# 37. The NK cell granule protein NKG7 regulates cytotoxic granule exocytosis and inflammation, Nature immunology, 2019
+# 38. https://en.wikipedia.org/wiki/GNLY
+# 39. Perforin and granzymes- function, dysfunction and human pathology, Nature review immunology, 2015
+# 40. Runx3 and T-box proteins cooperate to establish the transcriptional program of effector CTLs. J. Exp. Med. 206, 51–59 (2009).
+# 41. Mol. Cell. Biol. 7, 4472–4481 (1987). This paper identified CD28 as a co‐stimulator that could amplify TCR signalling to induce proliferation and IL‐2 production
+# 42. CD28 and CTLA-4 coreceptor expression and signal transduction. Immunol. Rev. 229, 12–26 (2009).
+# 43. T cell costimulatory receptor CD28 is a primary target for PD-1–mediated inhibition, Science 2017
+# 44. The storm has cleared- lessons from the CD28 superagonist TGN1412 trial, Nature reviews immunology, 2012
+# 45. https://blog.crownbio.com/lag3-a-promising-new-cancer-immunotherapy-target
+# 46. Transcriptional analysis of HIV-specific CD8+ T cells shows that PD-1 inhibits T cell function by upregulating BATF, Nature medicine 2010
+# 47. B7-1 and B7-2 selectively recruit CTLA-4 and CD28 to the immunological synapse. Immunity 21, 401–413 (2004).
+# 48. Programmed cell death 1 forms negative costimulatory microclusters that directly inhibit T cell receptor signaling by recruiting phosphatase SHP2. J. Exp. Med. 209, 1201–1217 (2012).
+# 49. T cell Ig and mucin domain- containing protein 3 is recruited to the immune synapse, disrupts stable synapse formation, and associates with receptor phosphatases. J. Immunol. 192, 782–791 (2014).
+# 50. Molecular and cellular insights into T cell exhaustion, Nature reviews immunology 2015
+# 51. Clinical blockade of PD1 and LAG3 — potential mechanisms of action, Nature reviews immunology, 2014
+# 52. Ejrnaes, M. et al. Resolution of a chronic viral infection after interleukin-10 receptor blockade. J. Exp. Med. 203, 2461–2472 (2006).
+# 53. Landscape and Dynamics of Single Immune Cells in Hepatocellular Carcinoma, Cell, 2019
+# 54. Infected CD8α– dendritic cells are the predominant source of IL-10 during establishment of persistent viral infection. Proc. Natl Acad. Sci. USA 109, 14116–14121 (2012).
+# 55. Programmed death-1-induced interleukin-10 production by monocytes impairs CD4+ T cell activation during HIV infection. Nat. Med. 16, 452–459 (2010).
+# 56. IL-10 and PD-L1 operate through distinct pathways to suppress T-cell activity during persistent viral infection, PNAS, 2008
+# 57. PD-L1 blockade synergizes with IL-2 therapy in reinvigorating exhausted T cells. J. Clin. Invest. 123, 2604–2615 (2013).
+# 58. A Transcriptionally Distinct CXCL13þCD103þCD8þ T-cell Population Is Associated with B-cell Recruitment and Neoantigen Load in Human Cancer, cancer immunology research 2019
+# 59. https://www.genecards.org/cgi-bin/carddisp.pl?gene=FCGR3A
+# 60. https://www.genecards.org/cgi-bin/carddisp.pl?gene=FGFBP2
+# 61. http://en.wikipedia.org/wiki/Common_gamma_chain
+# 62. CYTOKINE CONTROL OF MEMORY T-CELL DEVELOPMENT AND SURVIVAL, Nature reviews immunology, 2003
+# 63. The integration of T cell migration, differentiation and function, Nature reviews immunology, 2013
+# 64. https://www.genecards.org/cgi-bin/carddisp.pl?gene=ANXA1
+# 65. IL-17 and Th17 Cells, annual reviews immunology, 2009
+# 66. Tumor-Infiltrating Regulatory T-cell Accumulation in the Tumor Microenvironment Is Mediated by IL33/ST2 Signaling, cancer immunology research 2020
+# 67. ST2 as checkpoint target for colorectal cancer immunotherapy, JCI insight, 2019
+# 68. Dissecting the multicellular ecosystem of metastatic melanoma by single-cell RNA-seq, Science, 2016
+# 69. Tumor-Derived IL33 Promotes Tissue-Resident CD8 + T Cells and Is Required for Checkpoint Blockade Tumor Immunotherapy,  cancer immunology research 2020
+# 70. Mutations Associated with Acquired Resistance to PD-1 Blockade in Melanoma, NEJM, 2016
+# 71. S-100 protein: is it useful as a tumour marker in diagnostic immunocytochemistry?, 1980
+# 72. interferon-Gamma at the Crossroads of Tumor immune Surveillance or evasion, frontieres immunology, 2018
+# 73. Resistance to checkpoint blockade therapy through inactivation of antigen presentation, Nature communication, 2017
+# 74. https://www.genecards.org/cgi-bin/carddisp.pl?gene=HMGB2
+# 75. https://www.genecards.org/cgi-bin/carddisp.pl?gene=CKS1B
+# 76. Single-cell profiling of breast cancer T cells reveals a tissue-resident memory subset associated with improved prognosis, Nature medicine, 2018
+# 77. https://www.genecards.org/cgi-bin/carddisp.pl?gene=CCL5
+# 78. https://www.sciencedirect.com/topics/neuroscience/ccl5
